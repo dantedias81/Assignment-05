51918aaf81f7f7cc002f87e4e6ccc7bec940a2a5
Dante <dantedias81@gmail.com>
2019-11-06 09:50:42 -0500

    Created a Script
    Displays UNRATE as a Data Table

---
+++ Unemployment_Table.R
@@ -0,0 +1,6 @@
+#Dante Dias
+#This script will take a set of data and display it in a data table
+
+if (!require("data.table")) install.packages("data.table")
+library("data.table")
+DF <- fread("UNRATE.csv", header="auto", data.table=TRUE)
