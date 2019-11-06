24343c8eaa6c7e02a7be41596eb114d30a0b8248
Dante <dantedias81@gmail.com>
2019-11-06 10:55:05 -0500

    Edited Again

--- Unemployment_Table.R
+++ Unemployment_Table.R
@@ -3,4 +3,12 @@
 
 if (!require("data.table")) install.packages("data.table")
 library("data.table")
-DF <- fread("UNRATE.csv", header="auto", data.table=FALSE)
+header <- read.table("UNRATE.csv", header = TRUE,
+                     sep=",", nrow = 1)
+DF <- fread("UNRATE.csv",
+                                 skip=1, sep=",",header=FALSE,
+                                 data.table=FALSE)
+setnames(Unemployment, colnames(header))
+rm(header)
+rm(list=ls(all=TRUE))
+cat("\014") 
